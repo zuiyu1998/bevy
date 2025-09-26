5ac83b1f65dfb9d9c3f5beba2d384fdcdd9d4004
zuiyu1998 <1542844298@qq.com>
2025-09-26 13:31:24 +0800

    Add PassNode

--- crates/bevy_render/src/frame_graph/pass_node.rs
+++ crates/bevy_render/src/frame_graph/pass_node.rs
@@ -1,1 +1,25 @@
-pub struct PassNode {}
+use crate::frame_graph::{IndexHandle, Pass, ResourceHandle, ResourceNode};
+
+pub struct PassNode {
+    pub name: String,
+    pub index: IndexHandle<PassNode>,
+    pub writes: Vec<ResourceHandle>,
+    pub reads: Vec<ResourceHandle>,
+    pub resource_request_array: Vec<IndexHandle<ResourceNode>>,
+    pub resource_release_array: Vec<IndexHandle<ResourceNode>>,
+    pub pass: Option<Pass>,
+}
+
+impl PassNode {
+    pub fn new(name: &str, index: IndexHandle<PassNode>) -> Self {
+        Self {
+            name: name.to_string(),
+            index,
+            writes: Default::default(),
+            reads: Default::default(),
+            resource_request_array: Default::default(),
+            resource_release_array: Default::default(),
+            pass: Default::default(),
+        }
+    }
+}
