607dbfe03021bd3602a2f5687060a0ffa0133c6a
zuiyu1998 <1542844298@qq.com>
2026-01-31 22:12:14 +0800

    feat: add ResourceNode

--- crates/bevy_render/src/frame_graph/mod.rs
+++ crates/bevy_render/src/frame_graph/mod.rs
@@ -3,9 +3,11 @@ mod index_handle;
 mod resource_table;
 mod pipeline_container;
 mod pass;
+mod resource_node;
 
 pub use transient_resource::*;
 pub use index_handle::*;
 pub use resource_table::*;
 pub use pipeline_container::*;
 pub use pass::*;
+pub use resource_node::*;
