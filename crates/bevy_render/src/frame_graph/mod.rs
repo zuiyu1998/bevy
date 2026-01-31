17accf6e94ac5c9747e09636570d0cc1985fbfce
zuiyu1998 <1542844298@qq.com>
2026-01-31 22:14:38 +0800

    feat: add PassNode

--- crates/bevy_render/src/frame_graph/mod.rs
+++ crates/bevy_render/src/frame_graph/mod.rs
@@ -4,6 +4,7 @@ mod resource_table;
 mod pipeline_container;
 mod pass;
 mod resource_node;
+mod pass_node;
 
 pub use transient_resource::*;
 pub use index_handle::*;
@@ -11,3 +12,4 @@ pub use resource_table::*;
 pub use pipeline_container::*;
 pub use pass::*;
 pub use resource_node::*;
+pub use pass_node::*;
