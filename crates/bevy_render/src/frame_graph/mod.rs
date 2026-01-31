4c1d0d097cb5899590e960850bc68f9d511cf7c7
zuiyu1998 <1542844298@qq.com>
2026-01-31 22:08:59 +0800

    feat: add Pass

--- crates/bevy_render/src/frame_graph/mod.rs
+++ crates/bevy_render/src/frame_graph/mod.rs
@@ -1,3 +1,11 @@
 mod transient_resource;
+mod index_handle;
+mod resource_table;
+mod pipeline_container;
+mod pass;
 
 pub use transient_resource::*;
+pub use index_handle::*;
+pub use resource_table::*;
+pub use pipeline_container::*;
+pub use pass::*;
